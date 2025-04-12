72b6be03627fc9e74f2dce0288a0fa337daa3842
cand7655 <rudtjr7655@naver.com>
2025-04-12 12:17:49 +0900

    Delete cand5 directory

--- cand5/cand5.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = "my-checkov-test-bucket"
-  acl    = "public-read" # <-- 이 부분이 checkov에 걸릴 가능성 높음 (Critical)
-}
